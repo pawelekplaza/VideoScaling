e419b28afe664e080cc30c9b1b2c96728b2e9fd9
pawelekplaza <pawelekplaza@gmail.com>
2016-11-29 07:21:44 +0100

    ProgressBarArguments - event args class added

---
+++ VideoScaling/Events/ProgressBarArguments.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoScaling.Events
+{
+    public class ProgressBarArguments : EventArgs
+    {
+        public double PBValue { get; set; }
+        public string TimeProceeded { get; set; }
+    }
+}
